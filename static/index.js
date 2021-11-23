4395c5119bb859377b3daa76c85b4bf6a8aee336
VCNguen <vcnguen@mai.ru>
2021-11-24 00:19:26 +0300

    first commit

---
+++ static/index.js
@@ -0,0 +1,16 @@
+alert('test js');
+var detail = document.getElementsByClassName("slide2");
+detail.scrollIntoView();
+
+function toDetail(){
+var detail = document.getElementsByClassName("slide2");
+detail.scrollIntoView();
+}
+// function toProduct(){
+//     alert("to product");
+//     var detail = document.getElementsByClassName("slide3");
+//     detail.scrollIntoView();
+// }
+function toContact(){
+
+}
